8355835f34499c0ae797479b016ff6e4696ab106
João Marcelo Daltro <90789618+celinhodaltro@users.noreply.github.com>
2024-11-06 16:34:08 -0300

    Adicionar Dao ao projeto principal

--- TesteDengineCore/Program.cs
+++ TesteDengineCore/Program.cs
@@ -1,3 +1,57 @@
 ﻿//var db = ConsoleServiceProvider.Instance.CreateDbExercicio4();
 
-Console.WriteLine("Db Criado com sucesso");
+
+using Microsoft.EntityFrameworkCore;
+using Server.DataAcessObject;
+using Server.Entities;
+
+using (var context = new AppDbContext(new DbContextOptionsBuilder<AppDbContext>().Options))
+{
+
+
+    // Criando uma fatura de teste
+    var newfatura = new Fatura
+    {
+        FaturaId = 1,
+        Cliente = "Cliente Exemplo",
+        Data = DateTime.Now,
+        FaturaItem = new List<FaturaItem>
+    {
+        new FaturaItem
+        {
+            FaturaItemId = 1,
+            FaturaId = 1,
+            Ordem = 1,
+            Valor = 100.50
+        },
+        new FaturaItem
+        {
+            FaturaItemId = 2,
+            FaturaId = 1,
+            Ordem = 2,
+            Valor = 200.75
+        },
+        new FaturaItem
+        {
+            FaturaItemId = 3,
+            FaturaId = 1,
+            Ordem = 3,
+            Valor = 50.00
+        }
+    }
+    };
+
+    context.Fatura.Add(newfatura);
+    context.SaveChanges();
+
+    var fatura = context.Fatura.FirstOrDefault();
+    Console.WriteLine($"Fatura ID: {fatura.FaturaId}");
+    Console.WriteLine($"Cliente: {fatura.Cliente}");
+    Console.WriteLine($"Data: {fatura.Data}");
+    Console.WriteLine("Itens da Fatura:");
+    foreach (var item in fatura.FaturaItem)
+    {
+        Console.WriteLine($" - Item {item.Ordem}: {item.Valor:C}");
+    }
+
+}
