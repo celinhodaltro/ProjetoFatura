1e3c6236d32952e01ebbe43bd141d74ac317fadd
João Marcelo Daltro <90789618+celinhodaltro@users.noreply.github.com>
2024-11-06 17:25:05 -0300

    Mudar projeto para ASP.NET (MVC)

---
+++ Interface/Interface.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Interface.App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
