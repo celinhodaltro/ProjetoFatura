55b8e2183dc9bd9ed7330c45da2b8dbfc1ee19e3
João Marcelo Daltro <90789618+celinhodaltro@users.noreply.github.com>
2024-11-06 16:07:32 -0300

    - Rename: dbContext4 to AppContext

---
+++ Server.DataAcessObject/AppContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace TesteDengine;
+
+public class AppContext: DbContext
+{
+    public AppContext(DbContextOptions<DbExercicio4> options)
+        : base(options)
+    {
+
+    }
+
+    public DbSet<Fatura> Fatura { get; set; }
+    public DbSet<FaturaItem> FaturaItem{ get; set; }
+        
+}
