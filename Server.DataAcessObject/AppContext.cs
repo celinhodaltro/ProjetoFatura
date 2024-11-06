1a86333d7ca2c5e41120942fb6591ed954095eb8
João Marcelo Daltro <90789618+celinhodaltro@users.noreply.github.com>
2024-11-06 16:17:53 -0300

    - arch: adicionar OnConfiguring ao dbcontext

--- Server.DataAcessObject/AppContext.cs
+++ Server.DataAcessObject/AppContext.cs
@@ -10,6 +10,11 @@ public class AppContext: DbContext
 
     }
 
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+    }
+
     public DbSet<Fatura> Fatura { get; set; }
     public DbSet<FaturaItem> FaturaItem{ get; set; }
         
