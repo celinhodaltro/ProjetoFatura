f21f8751feb17b19921ebf4f24fe1e11aa05a649
João Marcelo Daltro <90789618+celinhodaltro@users.noreply.github.com>
2024-11-06 16:34:29 -0300

    - Rename: Renomeiar AppDbContext;

---
+++ Server.DataAcessObject/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Server.Entities;
+
+namespace Server.DataAcessObject;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured) 
+        {
+            optionsBuilder.UseInMemoryDatabase("MemoryDatabase");
+        }
+    }
+
+    public DbSet<Fatura> Fatura { get; set; }
+    public DbSet<FaturaItem> FaturaItem { get; set; }
+}
