791bd8ee72a139d40fabdecdf93e43995edfc92b
João Marcelo Daltro <90789618+celinhodaltro@users.noreply.github.com>
2024-11-07 10:36:07 -0300

    Criar defautlDb

---
+++ Server/Server.Entities/Entities/Default/DefaultDb.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Entities.Entities.Default
+{
+    public class DefaultDb
+    {
+
+        public int Id { get; set; }
+
+        public virtual bool Validate()
+        {
+            return true;
+        }
+    }
+}
