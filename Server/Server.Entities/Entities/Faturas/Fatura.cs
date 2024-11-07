4728ebb6a7b21232f2af49dbd7dca21b861e3571
João Marcelo Daltro <90789618+celinhodaltro@users.noreply.github.com>
2024-11-07 10:38:13 -0300

    Adicionar validador á Fatura

--- Server/Server.Entities/Entities/Faturas/Fatura.cs
+++ Server/Server.Entities/Entities/Faturas/Fatura.cs
@@ -1,5 +1,6 @@
 ﻿using Server.Entities.Entities.Default;
 using System.ComponentModel.DataAnnotations;
+using System.Text;
 
 namespace Server.Entities;
 
@@ -17,5 +18,19 @@ public class Fatura :DefaultDb
 
     public virtual ICollection<FaturaItem> FaturaItem { get; set; }
 
+    public override bool Validate()
+    {
+        StringBuilder Result = new("");
+
+        if (this.Cliente is null)
+            Result.Append("O Cliente é obrigatorio;");
+
+        var Errors = Result.ToString();
+
+        if (String.IsNullOrEmpty(Errors))
+            return true;
+        else
+            throw new InvalidDataException(Result.ToString());
+    }
 
 }
