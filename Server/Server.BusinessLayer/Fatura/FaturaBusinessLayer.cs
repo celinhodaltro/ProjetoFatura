4f42133a31a36ce637bda3c7ceb37fd44c06026c
João Marcelo Daltro <90789618+celinhodaltro@users.noreply.github.com>
2024-11-07 10:48:57 -0300

    Adicionar metodos á FaturaBusinessLayer

--- Server/Server.BusinessLayer/Fatura/FaturaBusinessLayer.cs
+++ Server/Server.BusinessLayer/Fatura/FaturaBusinessLayer.cs
@@ -1,18 +1,54 @@
 ﻿using Server.DataAcessObject.Providers;
+using Server.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Server.BusinessLayer
 {
     public class FaturaBusinessLayer
     {
-        public FaturaItemProvider? FaturaItemProvider { get; set; }
-        public FaturaProvider? FaturaProvider { get; set; }
+        private readonly FaturaProvider _faturaProvider;
+        private readonly FaturaItemProvider _faturaItemProvider;
 
-        
+        public FaturaBusinessLayer(FaturaProvider faturaProvider, FaturaItemProvider faturaItemProvider)
+        {
+            _faturaProvider = faturaProvider;
+            _faturaItemProvider = faturaItemProvider;
+        }
+
+        public async Task AdicionarFatura(Fatura fatura)
+        {
+            if (!fatura.Validate())
+                throw new Exception("A fatura não é válida.");
+
+            await _faturaProvider.Insert(fatura);
+        }
+
+        public async Task AdicionarFaturaItem(int faturaId, FaturaItem item)
+        {
+            var fatura = await _faturaProvider.GetFaturaWithItens(faturaId);
+            item.Validate();
+            
+
+            fatura.FaturaItem.Add(item);
+            await _faturaProvider.Update(fatura); 
+        }
+
+        public async Task ExcluirFatura(int faturaId)
+        {
+            var fatura = await _faturaProvider.Get(faturaId);
+            if (fatura != null)
+                await _faturaProvider.Delete(fatura);
+        }
+
+        public async Task ExcluirFaturaItem(int faturaItemId)
+        {
+            var faturaItem = await _faturaItemProvider.Get(faturaItemId);
+            if (faturaItem != null)
+                await _faturaItemProvider.Delete(faturaItem);
+        }
 
     }
 }
